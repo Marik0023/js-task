8e85d72900cd2435d0b612830931e450bc763252
Marik0023 <70592085+Marik0023@users.noreply.github.com>
2020-09-21 16:53:01 +0300

    Create task.js

---
+++ task.js
@@ -0,0 +1,18 @@
+https://www.codewars.com/kata/convert-a-string-to-an-array/train/javascript
+
+function stringToArray(string){
+  var str = 'Robin, Singh';
+  var re = /:\s|Robin, Singh\s/;
+  var str = 'I, love, arrays, they, are, my, favorite';
+  var re = /:\s|I, love, arrays, they, are, my, favorite\s/;
+  var ar = str.split(re);
+  console.log( ar );
+}
+
+https://www.codewars.com/kata/57f781872e3d8ca2a000007e/train/javascript
+function maps(x){
+  var numbers = [1, 2, 3];
+  var doubles = numbers.map(function(num) 
+  return num * 2; 
+  )};                         
+}
